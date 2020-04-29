7f1437129a2fed53221b920c5852dceef2d55306
PaulaSanchezR <paulasanchez.restrepo@gmail.com>
2020-04-29 15:58:44 -0400

    create folder

---
+++ Store/reducers/products.js
@@ -0,0 +1,12 @@
+import PRODUCTS from '../../data/dummy-data'
+
+const initialState={
+    availableProducts: [],  // all products
+    userProducts: [],  // products that the user created 
+
+
+};
+// Redux 
+export default (state = initialState, action ) =>{
+    return state
+}
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,12 @@
+// this sreen show the product when the app load list of all the products
+
+
+import React from 'react';
+import { FlatList } from 'react-native'
+
+
+const ProductsOverviewScreen = props => {
+    return <FlatList />
+}
+
+export default ProductsOverviewScreen
