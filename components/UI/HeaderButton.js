39b25ba2b2f64037be97da17f76042fae60f59b9
PaulaSanchezR <paulasanchez.restrepo@gmail.com>
2020-05-05 17:33:24 -0400

    adding the cart icon

--- components/UI/HeaderButton.js
+++ components/UI/HeaderButton.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { HeaderButton } from 'react-navigation-heade-buttons'
+import { Platform } from 'react-native'
+import { HeaderButton } from 'react-navigation-header-buttons'
 import { Ionicons } from '@expo/vector-icons'
 
 import Colors from '../../constants/Colors'
 
 
 const CustomeHeaderButton = props =>{
+// we use forward the props 
+    return (
+        <HeaderButton 
+            {...props} 
+            IconComponent={Ionicons} 
+            iconSize={23} 
+            color={Platform.OS === 'android' ? 'white' : Colors.primary}
+            
+            />)
 
-    
-
-}
+};
 
 export default CustomeHeaderButton;
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -2,10 +2,12 @@
 
 
 import React from 'react';
-import { FlatList, Text } from 'react-native'
+import { FlatList, Text , Platform} from 'react-native'
 import { useSelector , useDispatch} from 'react-redux'
 import ProductItem from '../../components/shop/ProductItem'
 import Product from '../../models/products';
+import { HeaderButtons, Item} from 'react-navigation-header-buttons'
+import HeaderButton from '../../components/UI/HeaderButton'
 import * as cartActions  from '../../Store/actions/cart'
 
 
@@ -49,7 +51,18 @@ const ProductsOverviewScreen = props => {
 }
 
 ProductsOverviewScreen.navigationOptions ={
-    headerTitle: 'All Products'
+    headerTitle: 'All Products',
+    headerRight:(
+        <HeaderButtons HeaderButtonComponent={HeaderButton} >
+        <Item 
+            title='Cart' 
+            iconName={Platform.OS ==='android' ? 'md-cart' : 'ios-cart'}
+            onPress={()=>{
+
+            }}
+            />
+    </HeaderButtons>
+    ) 
 }
 
 export default ProductsOverviewScreen            
