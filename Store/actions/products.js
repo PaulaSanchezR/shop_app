f56a0e9e037bc47caccd8640761da85aecf833d6
PaulaSanchezR <paulasanchez.restrepo@gmail.com>
2020-05-18 19:29:05 -0400

    firebase

--- Store/actions/products.js
+++ Store/actions/products.js
@@ -8,12 +8,32 @@ export const deleteProduct = productId =>{
 }
 
 export const createProduct = (title, description, imageUrl, price) =>{
-    return { type: CREATE_PRODUCT, productData:{
-        title:title,
-        description:description,
-        imageUrl, // modern javascritp
-        price
-    }
+    // thanks to reduxthunk
+    return async dispatch => {
+// before we can execute any async code we want!!
+// fetch return a promese
+        const response = await fetch('https://shop-app-7b156.firebaseio.com/products.json', // fetch is use to send post request or put request any kind or HTTP request
+        //                                          product.json is just a firebase thing you can create any json file like this
+                {
+                  method: 'POST',
+                  headers:{
+                      'Content-Type':'application/json'
+                  },
+                  body: JSON.stringify({
+                      title,
+                      description,
+                      imageUrl,
+                      price
+                  })
+                })
+        dispatch ({ type: CREATE_PRODUCT, productData:{
+            title:title,
+            description:description,
+            imageUrl, // modern javascritp
+            price
+        }
+
+    })
   }
 }
 
