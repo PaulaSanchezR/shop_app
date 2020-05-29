2d0de6d2393b4441fb6a11b342c641beb76d720d
PaulaSanchezR <paulasanchez.restrepo@gmail.com>
2020-05-29 19:42:12 -0400

    Authentication screen

---
+++ screens/user/AuthScreen.js
@@ -0,0 +1,16 @@
+// This screen will be present a login  or sign up
+
+import React from 'react'
+import { ScrollView, View, StyleSheet, KeyboardAvoidingView} from 'react-native'
+
+import Input from '../../components/UI/Input'
+
+const AuthScreen = props =>{
+
+};
+
+const styles=StyleSheet.create({
+
+});
+
+export default AuthScreen;
